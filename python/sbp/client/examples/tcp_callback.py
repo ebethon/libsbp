57d831a3ed802d7f917235f31c39173fbaf7573d
Etienne Bethon <etiennebethon@gmail.com>
2019-07-15 18:59:23 -0700

    update examples

--- python/sbp/client/examples/tcp_callback.py
+++ python/sbp/client/examples/tcp_callback.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2015 Swift Navigation Inc.
-# Contact: Fergus Noble <fergus@swiftnav.com>
+# Copyright (C) 2017 Swift Navigation Inc.
+# Contact: Swift Navigation <dev@swiftnav.com>
 #
 # This source is subject to the license found in the file 'LICENSE' which must
 # be be distributed together with this source. All other rights reserved.
@@ -13,10 +13,10 @@ reading SBP messages from a serial port, decoding BASELINE_NED messages and
 printing them out.
 """
 
-from sbp.client.drivers.pyserial_driver import PySerialDriver
+import argparse
+from sbp.client.drivers.network_drivers import TCPDriver
 from sbp.client import Handler, Framer
 from sbp.navigation import SBP_MSG_POS_LLH
-import argparse
 
 
 def position_llh_callback(sbp_msg, **metadata):
@@ -27,15 +27,19 @@ def main():
     parser = argparse.ArgumentParser(
         description="Swift Navigation SBP Example.")
     parser.add_argument(
+        "-a",
+        "--host",
+        default='localhost',
+        help="specify the host address.")
+    parser.add_argument(
         "-p",
         "--port",
-        default=['/dev/ttyUSB0'],
-        nargs=1,
-        help="specify the serial port to use.")
+        default=55555,
+        help="specify the port to use.")
     args = parser.parse_args()
 
-    # Open a connection to Piksi using the default baud rate (1Mbaud)
-    with PySerialDriver(args.port[0], baud=115200) as driver:
+    # Open a connection to Piksi using TCP
+    with TCPDriver(args.host, args.port) as driver:
         with Handler(Framer(driver.read, None, verbose=True)) as source:
             source.add_callback(position_llh_callback, SBP_MSG_POS_LLH)
 
